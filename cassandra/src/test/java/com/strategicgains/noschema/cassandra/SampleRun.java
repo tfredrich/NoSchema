78140402b9aedf0e10f8dd0addf933fad62e594d
Todd Fredrich <tfredrich@gmail.com>
2024-02-15 16:08:04 -0700

    Added tests for both BSON and GSON ObjectCodecs to Cassandra.

--- cassandra/src/test/java/com/strategicgains/noschema/cassandra/SampleRun.java
+++ cassandra/src/test/java/com/strategicgains/noschema/cassandra/SampleRun.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.strategicgains.noschema.Identifier;
+import com.strategicgains.noschema.bson.BsonObjectCodec;
 import com.strategicgains.noschema.cassandra.document.DocumentTableSchemaProvider;
 import com.strategicgains.noschema.cassandra.schema.SchemaRegistry;
 import com.strategicgains.noschema.document.ObjectCodec;
@@ -20,89 +21,18 @@ import com.strategicgains.noschema.gson.GsonObjectCodec;
 public class SampleRun {
 
 	private static final String FLOWERS_BY_NAME = "by_name";
-	private static final ObjectCodec<Flower> CODEC = new GsonObjectCodec<>();
-
+	private static final ObjectCodec<Flower> GSON_CODEC = new GsonObjectCodec<>();
+	private static final ObjectCodec<Flower> BSON_CODEC = new BsonObjectCodec<>();
 
 	public static void main(String[] args)
 	throws KeyDefinitionException, InvalidIdentifierException, DuplicateItemException, ItemNotFoundException
 	{
 		CqlSession session = createCassandraSession();
-		String keyspace = "sample_run";
-
-		PrimaryTable flowersTable = new PrimaryTable(keyspace, "flowers", "id:UUID unique")
-			.withView(FLOWERS_BY_NAME, "(account.id as account_id:UUID), name:text unique");
-
-		SchemaRegistry schemas = SchemaRegistry.keyspace(keyspace);
-		flowersTable.stream().forEach(v -> schemas.withProvider(new DocumentTableSchemaProvider(v)));
-		schemas.initializeAll(session)
-			.exportInitializeAll();
-
-		CassandraNoSchemaRepository<Flower> flowers = new CassandraNoSchemaRepository<>(session, flowersTable, CODEC);
-		flowers.withObserver(new SampleObserver());
-		flowers.ensureTables();
-
-		UUID id = UUID.fromString("8dbac965-a1c8-4ad6-a043-5f5a9a5ee8c0");
-		UUID accountId = UUID.fromString("a87d3bff-6997-4739-ab4e-ded0cc85700f");
-		Flower flower = instantiateFlower(accountId, id);
 
 		try
 		{
-			System.out.println("*** READ ID (Not Found) ***");
-			try
-			{
-				flowers.read(flower.getIdentifier());
-				throw new RuntimeException("FAILED: Expected ItemNotFoundException (by ID)");
-			}
-			catch (ItemNotFoundException e)
-			{
-				System.out.println("Recieved expected exception: ItemNotFoundException: " + e.getMessage());
-			}
-
-			System.out.println("*** READ NAME (Not Found) ***");
-			try
-			{
-				flowers.read(FLOWERS_BY_NAME, new Identifier(accountId, "rose"));
-				throw new RuntimeException("FAILED: Expected ItemNotFoundException (by name)");
-			}
-			catch (ItemNotFoundException e)
-			{
-				System.out.println("Recieved expected exception: ItemNotFoundException: " + e.getMessage());
-			}
-
-			System.out.println("*** CREATE ***");
-			Flower written = flowers.create(flower);
-			System.out.println(written.toString());
-
-			System.out.println("*** READ ID ***");
-			Flower read = flowers.read(new Identifier(id));
-			System.out.println(read.toString());
-
-			System.out.println("*** READ NAME ***");
-			read = flowers.read(FLOWERS_BY_NAME, new Identifier(accountId, "rose"));
-			System.out.println(read.toString());
-
-			System.out.println("*** CREATE DUPLICATE ***");
-			try
-			{
-				flowers.create(flower);
-				throw new RuntimeException("FAILED: Expected DuplicateItemException");
-			}
-			catch (DuplicateItemException e)
-			{
-				System.out.println("Recieved expected exception: DuplicateItemException: " + e.getMessage());
-			}
-
-			System.out.println("*** UPDATE ***");
-			Flower updated = new Flower(read);
-			updated.setName(read.getName() + "-updated");
-			updated.setIsBlooming(false);
-			updated.setColors(Arrays.asList("blue", "green", "yellow"));
-			updated = flowers.update(updated, read);
-			System.out.println(updated.toString());
-
-			System.out.println("*** RE-READ ***");
-			read = flowers.read(new Identifier(updated.getId()));
-			System.out.println(read.toString());
+			testBson(session, "sample_run_bson");
+			testGson(session, "sample_run_gson");
 		}
 		finally
 		{
@@ -128,5 +58,96 @@ public class SampleRun {
 		    .addContactPoint(new InetSocketAddress("0.0.0.0", 9042))
 		    .withLocalDatacenter("datacenter1")
 		    .build();
+	}
+
+	private static void testBson(CqlSession session, String keyspace)
+	{
+		SchemaRegistry.keyspace(keyspace);
+		PrimaryTable flowersTable = new PrimaryTable(keyspace, "flowers", "id:UUID unique")
+			.withView(FLOWERS_BY_NAME, "(account.id as account_id:UUID), name:text unique");
+		testCassandra(keyspace, session, flowersTable, BSON_CODEC);
+		SchemaRegistry.clear();
+	}
+
+	private static void testGson(CqlSession session, String keyspace)
+	{
+		SchemaRegistry.keyspace(keyspace);
+		PrimaryTable flowersTable = new PrimaryTable(keyspace, "flowers", "id:UUID unique")
+			.withView(FLOWERS_BY_NAME, "(account.id as account_id:UUID), name:text unique");
+		testCassandra(keyspace, session, flowersTable, GSON_CODEC);
+		SchemaRegistry.clear();
+	}
+
+	private static void testCassandra(String keyspace, CqlSession session, PrimaryTable flowersTable, ObjectCodec<Flower> codec)
+	{
+		SchemaRegistry schemas = SchemaRegistry.instance();
+		flowersTable.stream().forEach(v -> schemas.withProvider(new DocumentTableSchemaProvider(v)));
+		schemas.initializeAll(session)
+			.exportInitializeAll();
+
+		CassandraNoSchemaRepository<Flower> flowers = new CassandraNoSchemaRepository<>(session, flowersTable, codec);
+		flowers.withObserver(new SampleObserver());
+		flowers.ensureTables();
+
+		UUID id = UUID.fromString("8dbac965-a1c8-4ad6-a043-5f5a9a5ee8c0");
+		UUID accountId = UUID.fromString("a87d3bff-6997-4739-ab4e-ded0cc85700f");
+		Flower flower = instantiateFlower(accountId, id);
+
+		System.out.println("*** READ ID (Not Found) ***");
+		try
+		{
+			flowers.read(flower.getIdentifier());
+			throw new RuntimeException("FAILED: Expected ItemNotFoundException (by ID)");
+		}
+		catch (ItemNotFoundException e)
+		{
+			System.out.println("Recieved expected exception: ItemNotFoundException: " + e.getMessage());
+		}
+
+		System.out.println("*** READ NAME (Not Found) ***");
+		try
+		{
+			flowers.read(FLOWERS_BY_NAME, new Identifier(accountId, "rose"));
+			throw new RuntimeException("FAILED: Expected ItemNotFoundException (by name)");
+		}
+		catch (ItemNotFoundException e)
+		{
+			System.out.println("Recieved expected exception: ItemNotFoundException: " + e.getMessage());
+		}
+
+		System.out.println("*** CREATE ***");
+		Flower written = flowers.create(flower);
+		System.out.println(written.toString());
+
+		System.out.println("*** READ ID ***");
+		Flower read = flowers.read(new Identifier(id));
+		System.out.println(read.toString());
+
+		System.out.println("*** READ NAME ***");
+		read = flowers.read(FLOWERS_BY_NAME, new Identifier(accountId, "rose"));
+		System.out.println(read.toString());
+
+		System.out.println("*** CREATE DUPLICATE ***");
+		try
+		{
+			flowers.create(flower);
+			throw new RuntimeException("FAILED: Expected DuplicateItemException");
+		}
+		catch (DuplicateItemException e)
+		{
+			System.out.println("Recieved expected exception: DuplicateItemException: " + e.getMessage());
+		}
+
+		System.out.println("*** UPDATE ***");
+		Flower updated = new Flower(read);
+		updated.setName(read.getName() + "-updated");
+		updated.setIsBlooming(false);
+		updated.setColors(Arrays.asList("blue", "green", "yellow"));
+		updated = flowers.update(updated, read);
+		System.out.println(updated.toString());
+
+		System.out.println("*** RE-READ ***");
+		read = flowers.read(new Identifier(updated.getId()));
+		System.out.println(read.toString());
 	}
 }
