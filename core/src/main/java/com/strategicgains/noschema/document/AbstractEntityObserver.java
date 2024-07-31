3bea34d4ad5040a6341f7d9e170c6295b65eee2b
Todd Fredrich <tfredrich@gmail.com>
2024-07-31 16:07:57 -0400

    Added beforeRead() and javadoc.

--- core/src/main/java/com/strategicgains/noschema/document/AbstractEntityObserver.java
+++ core/src/main/java/com/strategicgains/noschema/document/AbstractEntityObserver.java
@@ -3,10 +3,18 @@
  */
 package com.strategicgains.noschema.document;
 
+import com.strategicgains.noschema.Identifiable;
+import com.strategicgains.noschema.Identifier;
+
 /**
+ * A do-nothing implementation of EntityObserver to enable extenders
+ * to simply override the method(s) of interest.
  * 
+ * @author Todd Fredrich
+ * @see EntityObserver
+ * @param <T> the type of entity to observe.
  */
-public abstract class AbstractEntityObserver<T>
+public abstract class AbstractEntityObserver<T extends Identifiable>
 implements EntityObserver<T>
 {
 	protected AbstractEntityObserver() 
@@ -32,6 +40,9 @@ implements EntityObserver<T>
 
 	@Override
 	public void beforeDelete(T entity) {}
+
+	@Override
+	public void beforeRead(Identifier id) {}
 
 	@Override
 	public void beforeUpdate(T entity) {}
--- core/src/main/java/com/strategicgains/noschema/document/EntityObserver.java
+++ core/src/main/java/com/strategicgains/noschema/document/EntityObserver.java
@@ -1,13 +1,72 @@
 package com.strategicgains.noschema.document;
 
-public interface EntityObserver<T>
+import com.strategicgains.noschema.Identifiable;
+import com.strategicgains.noschema.Identifier;
+
+/**
+ * EntityObserver is an interface for classes that wish to observe the lifecycle
+ * of entities.
+ * 
+ * @param <T> the type of entity to observe.
+ */
+public interface EntityObserver<T extends Identifiable>
 {
+	/**
+	 * Called after an entity is created.
+	 * 
+	 * @param entity the entity that was created.
+	 */
 	public void afterCreate(T entity);
+
+	/**
+	 * Called after an entity is deleted.
+	 * 
+	 * @param entity the entity that was deleted.
+	 */
 	public void afterDelete(T entity);
+
+	/**
+	 * Called after an entity is read.
+	 * 
+	 * @param entity the entity that was read.
+	 */
 	public void afterRead(T entity);
+
+	/**
+	 * Called after an entity is updated.
+	 * 
+	 * @param entity the entity that was updated.
+	 */
 	public void afterUpdate(T entity);
 
+	/**
+	 * Called before an entity is created. Any changes made to the entity will be
+	 * persisted.
+	 * 
+	 * @param entity the entity that will be created.
+	 */
 	public void beforeCreate(T entity);
+
+	/**
+	 * Called before an entity is deleted.
+	 * 
+	 * @param entity the entity that will be deleted.
+	 */
 	public void beforeDelete(T entity);
+
+	/**
+	 * Called before an entity is read. Any changes made to the identifier will be
+	 * used to read the entity.
+	 * 
+	 * @param id the identifier of the entity that will be read.
+	 */
+	public void beforeRead(Identifier id);
+
+	/**
+	 * Called before an entity is updated. Any changes made to the entity will be
+	 * persisted.
+	 * 
+	 * @param entity the entity that will be updated.
+	 */
 	public void beforeUpdate(T entity);
 }
